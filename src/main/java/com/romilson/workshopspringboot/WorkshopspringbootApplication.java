5a903ed06a542b32e780693b1645535bb4dc2623
Romilson Junio <junio.romilson@gmail.com>
2022-02-05 17:58:04 -0300

    Primeiro Commit - Criacao Projeto

---
+++ src/main/java/com/romilson/workshopspringboot/WorkshopspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.romilson.workshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/romilson/workshopspringboot/WorkshopspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romilson.workshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
